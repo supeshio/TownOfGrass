3736ce2adaba85b349bcf3ad6f44ea6a09cb76dc
supeshio <117157354+supeshio@users.noreply.github.com>
2025-08-18 01:29:13 +0900

    Update TOG

--- TOG.cs
+++ TOG.cs
@@ -3,22 +3,24 @@ using BepInEx.Logging;
 using BepInEx.Unity.IL2CPP;
 using HarmonyLib;
 
-namespace TownOfGrass;
-
-[BepInPlugin(PluginName, PluginName, PluginVersion)]
-public class TOG : BasePlugin
+namespace TownOfGrass
 {
-    public const string Id = "supeshio.com.github";
-    public const string PluginName = "TownOfGrass";
-    public const string PluginVersion = "1.0.0";
-    public Harmony Harmony = new(Id);
-    internal new static ManualLogSource Logger;
 
-    public override void Load()
+    [BepInPlugin(Id, PluginName, PluginVersion)]
+    public class TOG : BasePlugin
     {
-        // Plugin startup logic
-        Logger = base.Log;
-        Log.LogInfo($"{PluginName} is loaded!");
-        //Harmony.PatchAll();
+        public const string Id = "supeshio.com.github";
+        public const string PluginName = "TownOfGrass";
+        public const string PluginVersion = "1.0.0";
+        public Harmony Harmony = new(Id);
+        internal new static ManualLogSource Log;
+
+        public override void Load()
+        {
+            // Plugin startup logic
+            Log = base.Log;
+            Log.LogInfo($"{PluginName} is loaded!");
+            Harmony.PatchAll();
+        }
     }
 }
