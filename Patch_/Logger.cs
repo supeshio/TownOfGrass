c510d1f95e7be89b155f4fbe4a02e32d2a32c6a1
supeshio <117157354+supeshio@users.noreply.github.com>
2025-09-10 21:21:20 +0900

    Update

--- Patch_/Logger.cs
+++ Patch_/Logger.cs
@@ -1,21 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace TownOfGrass
 {
     public static class Logger
     {
+        public static void Info(string text, string tag = "", [CallerMemberName] string callerMethod = "") => TOG.Log.LogInfo($"[{tag}:Info][{DateTime.Now}][{callerMethod}]{text}");
 
-        public static void Info(string text, string tag = "", [CallerMemberName] string callerMethod = "") => TOG.Logger.LogInfo($"[{tag}:Info][{DateTime.Now}][{callerMethod}]{text}");
-        public static void Warning(string text, string tag = "", [CallerMemberName] string callerMethod = "") => TOG.Logger.LogWarning($"[{tag}:Warning][{DateTime.Now}][{callerMethod}]{text}");
-        public static void Error(string text, string tag = "", [CallerMemberName] string callerMethod = "") => TOG.Logger.LogError($"[{tag}:Error][{DateTime.Now}][{callerMethod}]{text}");
-        public static void Fatel(string text, string tag = "", [CallerMemberName] string callerMethod = "") => TOG.Logger.LogFatal($"[{tag}:Fatel][{DateTime.Now}][{callerMethod}]{text}");
-        public static void Message(string text, string tag = "", [CallerMemberName] string callerMethod = "") => TOG.Logger.LogMessage($"[{tag}][{DateTime.Now}][{callerMethod}]{text}");
+        public static void Warning(string text, string tag = "", [CallerMemberName] string callerMethod = "") => TOG.Log.LogWarning($"[{tag}:Warning][{DateTime.Now}][{callerMethod}]{text}");
 
+        public static void Error(string text, string tag = "", [CallerMemberName] string callerMethod = "") => TOG.Log.LogError($"[{tag}:Error][{DateTime.Now}][{callerMethod}]{text}");
+
+        public static void Fatel(string text, string tag = "", [CallerMemberName] string callerMethod = "") => TOG.Log.LogFatal($"[{tag}:Fatel][{DateTime.Now}][{callerMethod}]{text}");
+
+        public static void Message(string text, string tag = "", [CallerMemberName] string callerMethod = "") => TOG.Log.LogMessage($"[{tag}][{DateTime.Now}][{callerMethod}]{text}");
     }
-
 }
